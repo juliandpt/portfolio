209328b470d599dac59eff14a4991f562e35b135
Julián de Pablo <juliandpt98@gmail.com>
2025-04-11 20:42:30 +0200

    perf: updated env variables call

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server'
+  output: "server",
+  env: {
+    schema: {
+      RESEND_API_KEY: envField.string({
+        context: "server",
+        access: "secret"
+      })
+    }
+  }
 })
--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,7 +1,8 @@
 import type { APIRoute } from "astro";
 import { Resend } from "resend";
+import { getSecret } from "astro:env/server";
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const resend = new Resend(getSecret("RESEND_API_KEY"));
 
 export const POST: APIRoute = async ({ request }) => {
   const { email, message } = await request.json();
