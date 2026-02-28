8c34c5856c5492bbd84a53c9af0a431caa2a849b
juliandpt <juliandpt98@gmail.com>
2026-02-28 20:56:14 +0100

    refactor: remove contact section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,12 @@ import { defineConfig, envField } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 import vercel from '@astrojs/vercel'
-import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
   output: 'server',
   adapter: vercel(),
-  env: {
-    schema: {
-      RESEND_API_KEY: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-    },
-  },
 })
