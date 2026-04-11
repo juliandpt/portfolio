81e4d91e9b4aaba74305c1dffda39fa4aea998c5
juliandpt <juliandpt98@gmail.com>
2026-04-11 19:33:14 +0200

    chore(deps): updated pnpm packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,10 @@
 import { defineConfig, envField } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
-import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server',
-  adapter: vercel(),
 })
