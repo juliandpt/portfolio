5ae56fcd3d2734773a6930fcd4d233c6ab1e0864
juliandpt <juliandpt98@gmail.com>
2026-05-23 20:40:19 +0200

    chore: format and lint project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, envField } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
